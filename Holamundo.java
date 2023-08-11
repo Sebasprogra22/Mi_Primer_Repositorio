4e78d25513dcad3e27aae991b9e5b8e4ad390da6
Sebasprogra22 <142031530+Sebasprogra22@users.noreply.github.com>
2023-08-11 09:59:09 -0500

    Add files via upload
    Mi primer repositorio

---
+++ Holamundo.java
@@ -0,0 +1,9 @@
+/*
+ * @author JohanSebastian
+ */
+//clase principal de java
+public class Holamundo {
+  public static void main(String[] args) {
+      System.out.println("Hola Johan Velasquez");
+  } 
+}
